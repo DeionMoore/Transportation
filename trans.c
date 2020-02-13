dc01c1be861005c5a30df9645aebcf3cd46c5604
DeionMoore <43355638+DeionMoore@users.noreply.github.com>
2020-02-13 11:16:05 -0500

    Add files via upload

---
+++ trans.c
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Welcome to the Transportation code! ");
+     int c;
+     char str[100];
+     int bpass=50;
+     int subpass = 45;
+     int ppride = 2;
+    printf("We have a couple questions to ask first. n\
+            are you a student or elderly? press 1 for yes and 2 for no");
+            scanf("%d", str, c);  
+        if(c = 1){
+           printf("You choose student or elderly");
+           if
+           }
+           else{
+               printf("You choose other");
+           }
+            
+            
+}
